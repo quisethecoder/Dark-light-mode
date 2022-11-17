d9d5b8ff3587e7b6a6fe9eb1cc2839d6676284b2
quisethecoder <marquise_dawkins@aol.com>
2022-11-16 22:56:55 -0500

    final edit

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-document.getElementById('dmbutton').addEventListener('click', function() {
-    const css = document.getElementById('cssfile');
-    const dmb = document.getElementById('dmbutton');
+document.getElementById("dmbutton").addEventListener("click", function() {
+    const css = document.getElementById("cssfile");
+    const dmb = document.getElementById("dmbutton");
 
-    if (dmb.innerHTML == 'Dark Mode') {
-        css.setAttribute('href', './css/darkmode.css');
+    if (dmb.innerHTML == "Dark Mode") {
+        css.setAttribute("href", "./css/darkmode.css");
+
+        document.getElementById("js").innerHTML = "Bring Back The Lights";
         
-        dmb.innerHTML = 'Light Mode'
-    }else{ css.setAttribute('href', './css/styles.css');
+        dmb.innerHTML = "Light Mode"
+    }else{ css.setAttribute("href", "./css/styles.css");
+
+    document.getElementById("js").innerHTML = "This is a test";
    
-    dmb.innerHTML = 'Dark Mode'
+    dmb.innerHTML = "Dark Mode"
     }
 
-    // document.getElementsByClassName("heading").innerHTML = "Bring Back The Lights";
-    // document.getElementsByClassName("heading").innerHTML = "This is a test";
+    
+  
 
     
 });
